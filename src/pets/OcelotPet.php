3d1b80f2a5b33ad21b1d08d7f140165dc4bccec3
Adam Le <adamsuper169@gmail.com>
2016-05-19 15:10:56 +0700

    Update OcelotPet.php

--- src/pets/OcelotPet.php
+++ src/pets/OcelotPet.php
@@ -5,9 +5,13 @@ namespace pets;
 class OcelotPet extends Pets {
 
 	const NETWORK_ID = 22;
+	
+	const DATA_CAT_TYPE = 18
+	
+	const TYPE_WILD = 0
 
-    public $width = 0.72;
-    public $height = 0.9;
+    public $width = 0.312;
+    public $height = 0.75;
 
 	public function getName() {
 		return "OcelotPet";
