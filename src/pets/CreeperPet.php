b3d05ab1ae20ca34b3365efcd3ff82c01810740e
Adam Le <adamsuper169@gmail.com>
2016-05-26 06:28:46 -0700

    last update

--- src/pets/CreeperPet.php
+++ src/pets/CreeperPet.php
@@ -6,13 +6,9 @@ class CreeperPet extends Pets {
 
 	const NETWORK_ID = 33;
 	
-	const DATA_SWELL_DIRECTION = 16;
-	const DATA_SWELL = 17;
-	const DATA_SWELL_OLD = 18;
-	const DATA_POWERED = 19;
-	
 	public $width = 0.4;
 	public $height = 0.75;
+	
 	public function getName() {
 		return "CreeperPet";
 	}
