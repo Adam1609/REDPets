954f359265f8d1c7dc9b8d6db79e008297cab13e
Adam Le <adamsuper169@gmail.com>
2016-05-26 03:39:57 -0700

    Create Cowpet.php

---
+++ src/pets/Cowpet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace pets;
+
+class CowPet extends Pets {
+	const NETWORK_ID = 42;
+	const NETWORK_ID = 11;
+	public $width = 0.3;
+	public $length = 0.9;
+	public $height = 1.8;
+	public function getName() {
+		return "CowPet";
+	}
+	public function getSpeed() {
+		return 1.2;
+	}
+}
